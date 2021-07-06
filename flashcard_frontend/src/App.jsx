d51aeb0cef1f24b1f4e061bd64210a5dff880916
Your Name <you@example.com>
2021-07-06 14:03:45 -0500

    flashcards are now displaying on the server

--- flashcard_frontend/src/App.jsx
+++ flashcard_frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'; 
 import CollectionsDisplay from './components/CollectionsDisplay/collections_display';
+import FlashcardDisplay from './components/FlashcardDisplay/flashcard_display';
 
 
 function App() {
   const [collections, setCollections] = useState([]);
+  const [flashcards, setFlashcards] = useState([]);
 
   useEffect(() => {
     getCollections();
+    getFlashcards();
   }, []);
   
   let getCollections = async () => {
@@ -20,10 +23,21 @@ function App() {
     }
   }
   
-  
+  let getFlashcards = async () => {
+    try{
+      let response = await axios.get('http://127.0.0.1:8000/flashcards/');
+      setFlashcards(response.data)
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
+
+
   return(
     <div>
-      <CollectionsDisplay collections={collections}/>
+      <CollectionsDisplay collections={collections} flashcard={flashcards}/>
+      <FlashcardDisplay flashcards={flashcards} />
     </div>
   );
 
--- flashcard_frontend/src/components/CollectionsDisplay/collections_display.jsx
+++ flashcard_frontend/src/components/CollectionsDisplay/collections_display.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 function CollectionsDisplay(props){
 
     let collections= props.collections.map(function(collection){
-        return <ul key={collection.id}>
-            {collection.name}
+        return<ul key={collection.id}>
+            {collection.id}-{collection.name}
         </ul>
     });
 
     return(
         <div className="container">
             <h1><u>Collections</u></h1>
-            <h3>{collections}</h3>
+            <h5>{collections}</h5>
         </div>
     )
 }
